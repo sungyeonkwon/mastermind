e48e4ed24a0b03113c82c009231e3cbd2b5cd801
Sung Kwon <sung@phntms.com>
2020-04-08 22:23:50 +0100

    Add event handlers for chatbox

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function ChatBox() {
+  const [value, setValue] = useState('');
+
+  const handleChange = event => {
+    setValue(event.target.value);
+  };
+
+  const handleKeyDown = event => {
+    if (event.key === 'Enter') {
+      // TODO: Push to ChatDocument, referenced from RoundDocument.
+    }
+  };
+
   return (
     <div className="chat-box">
       <div>
         <p>Chat line 1</p>
         <p>Chat line 2</p>
       </div>
-      <input />
+      <input
+        type="text"
+        value={value}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
     </div>
   );
 }
