37470880418e0e718006d87eae8952077ab4fb02
Sung Kwon <sung@phntms.com>
2020-04-11 23:02:20 +0100

    Add options for the pegs

--- src/components/OptionBox.js
+++ src/components/OptionBox.js
@@ -1,5 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import {COLOR_OPTIONS, CLUE_OPTIONS} from '../shared/config';
+
+const Options = styled.div`
+  border: 1px solid black;
+  padding: 10px;
+`;
+
+const ColorPeg = styled.div`
+  background: ${props => props.color};
+  border-radius: 50%;
+  border: 1px solid black;
+  display: inline-block;
+  height: 50px;
+  margin: 10px;
+  width: 50px;
+`;
 
 export default function OptionBox() {
-  return <div className="option-box">options</div>;
+  return (
+    <div className="option-box">
+      <Options>
+        Guess options:
+        {COLOR_OPTIONS.map((color, index) => (
+          <ColorPeg key={index} color={color}>
+            {color}
+          </ColorPeg>
+        ))}
+      </Options>
+      <Options>
+        Clue options:
+        {CLUE_OPTIONS.map((color, index) => (
+          <ColorPeg key={index} color={color}>
+            {color}
+          </ColorPeg>
+        ))}
+      </Options>
+    </div>
+  );
 }
--- src/shared/config.js
+++ src/shared/config.js
@@ -1,8 +1,19 @@
 export const GameConfig = {
+  clueCount: 2,
   clueSpotCount: 4,
-  colorCount: 6,
   guessSpotCount: 5,
+  rounds: 2,
   rowCount: 12,
   winningPoint: 10,
-  rounds: 2,
 };
+
+export const COLOR_OPTIONS = [
+  'red',
+  'blue',
+  'green',
+  'orange',
+  'brown',
+  'pink',
+];
+
+export const CLUE_OPTIONS = ['black', 'white'];
