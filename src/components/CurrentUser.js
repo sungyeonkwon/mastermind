79dc01c374c9a74ada3d869aca8107a1f62af6ad
Sung Kwon <sung@phntms.com>
2020-04-15 19:01:43 +0100

    Add basic style for auth components

--- src/components/CurrentUser.js
+++ src/components/CurrentUser.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import {signOut} from '../services/firebase';
 
-export default function CurrentUser({displayName, email, createdAt}) {
+const SectionStyle = styled.section`
+  background: rgba(0, 0, 0, .5);
+  color: white;
+  display: flex;
+  right: 0;
+  position: fixed;
+  top: 0;
+  height: 30px;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+
+  p {
+    padding: 0 10px;
+  }
+`;
+
+export default function CurrentUser({displayName}) {
   return (
-    <section>
-      <div>
-        <h2>{displayName}</h2>
-        <p>{email}</p>
-        <p>{createdAt}</p>
-      </div>
-      <div>
-        <button onClick={signOut}>Sign Out</button>
-      </div>
-    </section>
+    <SectionStyle>
+      <p>{displayName}</p>
+      <button onClick={signOut}>Sign Out</button>
+    </SectionStyle>
   );
 }
--- src/components/SingIn.js
+++ src/components/SingIn.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import {signInWithGoogle} from '../services/firebase';
 
+const ButtonStyle = styled.button`
+  align-items: center;
+  background: rgba(0, 0, 0, .5);
+  color: white;
+  display: flex;
+  height: 30px;
+  justify-content: center;
+  outline: none;
+  position: fixed;
+  right: 0;
+  top: 0;
+`;
+
 export default function SignIn() {
-  return <button onClick={signInWithGoogle}>Sign In With Google</button>;
+  return <ButtonStyle onClick={signInWithGoogle}>Sign In With Google</ButtonStyle>;
 }
